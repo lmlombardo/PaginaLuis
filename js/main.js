164edac035365d16b2aeedd8f93df1bbd4fca536
unknown <lm.lombardo@alumnos.upm.es>
2019-02-28 00:45:54 +0100

    Creada parte superior, falta tema de colores

--- js/main.js
+++ js/main.js
@@ -2,3 +2,31 @@ function redirection(url){
   var win = window.open(url,);
   win.focus();
 }
+
+function dameEstrellas(){
+loadJSON('https://api.github.com/repos/luislombardis/luislombardis.github.io',
+         function(data) { console.log(data); },
+         function(xhr) { console.error(xhr); }
+   );
+}
+
+
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    var myParsed = JSON.parse(xhr.responseText);
+                     document.getElementById("dameEstrellas").innerHTML = "Mis estrellas son "+myParsed.stargazers_count;
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
