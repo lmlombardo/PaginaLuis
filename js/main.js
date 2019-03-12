27272d1008813c13126d855d68cf99f24d3c1e36
unknown <lm.lombardo@alumnos.upm.es>
2019-03-12 18:33:42 +0100

    Bugs solucionados

--- js/main.js
+++ js/main.js
@@ -31,8 +31,7 @@ function scrollFunction(){
 }
 
 function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0; 
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 
